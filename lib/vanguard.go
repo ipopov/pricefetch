b01fa956b1ae157012f21a2f9c8066d37182de73
Your Name <popov.ivo@gmail.com>
2018-12-08 20:05:23 -0500

    Annotate error messages

--- lib/vanguard.go
+++ lib/vanguard.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	e "github.com/pkg/errors"
 	"golang.org/x/net/html/charset"
 	"net/http"
 	"regexp"
@@ -22,9 +23,9 @@ type VanguardFetcher struct {
 
 func (fund VanguardFund) get() (float64, error) {
 	client := http.Client{}
-	resp, err := client.Get(fmt.Sprintf("http://personal.vanguard.com/us/FundsRSS?FundId=%d", fund.Id))
+	resp, err := client.Get(fmt.Sprintf("https://personal.vanguard.com/us/FundsRSS?FundId=%d", fund.Id))
 	if err != nil {
-		return 0, err
+		return 0, e.Wrap(err, "fetching vg fund")
 	}
 	var rss struct {
 		Text []byte `xml:"channel>item>title"`
@@ -32,7 +33,7 @@ func (fund VanguardFund) get() (float64, error) {
 	d := xml.NewDecoder(resp.Body)
 	d.CharsetReader = charset.NewReaderLabel
 	if err = d.Decode(&rss); err != nil {
-		return 0, err
+		return 0, e.Wrap(err, "failed to decode")
 	}
 	capture_price := regexp.MustCompile(`^Price as of [0-9/]+: \$([^ ]+).*$`)
 	matches := capture_price.FindSubmatch(rss.Text)
@@ -49,8 +50,8 @@ func (f VanguardFetcher) Run() ([]Security, error) {
 	wg.Add(len(f.Funds))
 	for out_index, fund := range f.Funds {
 		go func(out_index int, fund VanguardFund) {
-			var price float64
-			price, errs[out_index] = fund.get()
+			price, err := fund.get()
+			errs[out_index] = e.Wrap(err, fmt.Sprintf("for fund %v", fund))
 			ret[out_index] = Security{fund.Name, price}
 			wg.Done()
 		}(out_index, fund)
