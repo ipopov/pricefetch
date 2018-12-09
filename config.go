13ab318613ea2006115a30cadb345797579eafc7
Your Name <popov.ivo@gmail.com>
2018-12-08 20:20:49 -0500

    Update config with new fund ids

--- config.go
+++ config.go
@@ -8,10 +8,12 @@ var config = []pf.SecurityFetcher{
 			{7555, "bond_cit"},
 			{7553, "extended_cit"},
 			{7554, "s_p_cit"},
-			{7556, "ex_us_cit"},
 			{1870, "vtpsx"},
 			{569, "vtiax"},
 			{585, "vtsax"},
+			{1457, "vdipx"},
+			{1865, "vemrx"},
+			{570, "vfwax"},
 		},
 	},
 	pf.IexFetcher{
